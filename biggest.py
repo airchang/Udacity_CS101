1e42b9324e455b4f53fd6108595fe801d57fac5b
airchang <33506678+airchang@users.noreply.github.com>
2018-04-13 10:26:01 -0700

    Update biggest.py

--- biggest.py
+++ biggest.py
@@ -10,7 +10,7 @@ def greatest(list_of_numbers):
     if len(list_of_numbers) ==0:
         return 0
     elif len(list_of_numbers) ==1:
-        return listof_numbers
+        return list_of_numbers[0]
     else:
         i = 1
         bigger = list_of_numbers[0]
