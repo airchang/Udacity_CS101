84c74e3cde044de23243fdd990ac329d36054c26
airchang <33506678+airchang@users.noreply.github.com>
2018-04-13 22:10:23 -0700

    Update and rename Mutating_list_unchanged.py to total_enrollment.py

--- Mutating_list_unchanged.py
+++
@@ -1,2 +0,0 @@
-#which of the folloiwng procedures leave the values passed as the input unchanged?
-def proc1(p):
---
+++ total_enrollment.py
@@ -0,0 +1,36 @@
+# Define a procedure, total_enrollment,
+# that takes as an input a list of elements,
+# where each element is a list containing
+# three elements: a university name,
+# the total number of students enrolled,
+# and the annual tuition fees.
+
+# The procedure should return two numbers,
+# not a string, 
+# giving the total number of students
+# enrolled at all of the universities
+# in the list, and the total tuition fees
+# (which is the sum of the number
+# of students enrolled times the
+# tuition fees for each university).
+
+udacious_univs = [['Udacity',90000,0]]
+
+usa_univs = [ ['California Institute of Technology',2175,37704],
+              ['Harvard',19627,39849],
+              ['Massachusetts Institute of Technology',10566,40732],
+              ['Princeton',7802,37000],
+              ['Rice',5879,35551],
+              ['Stanford',19535,40569],
+              ['Yale',11701,40500]  ]
+
+def total_enrollment(a):
+    sum_student = 0
+    sum_tuition = 0
+    i = 0
+    while i + 1 <= len(a):
+        sum_student = sum_student + a[i][1]
+        sum_tuition = sum_tuition + a[i][2] * a[i][1]
+        i = i + 1
+    return sum_student, sum_tuition
+
